51aa7810f58871c9013c45b6f311c6e5ea48eb5e
Undefined <Undefined>
2018-11-20 12:22:59 +0900

    solve: ALDS1_13_B.py

--- ALDS/ALDS1_13_B.py
+++ ALDS/ALDS1_13_B.py
@@ -16,25 +16,25 @@ start = time.time()
 # copy the below part and paste to the submission form.
 # ---------function------------
 from collections import deque
-import bisect
-
 CORRECT_STATE = "123456780"
 
+
 class PuzzleState:
+    __slots__ = ['state', 'spc_x', 'spc_y', 'depth']
+
     def __init__(self, state: str, depth=0):
         self.state = state
         index = state.find('0')
-        self.sp_x = index % 3
-        self.sp_y = index // 3
+        self.spc_x = index % 3
+        self.spc_y = index // 3
         self.depth = depth
-        self.limit = 0
 
-    def swap_state(self, x1, y1, x2, y2):
-        tmp = self.state[y2*3+x2]
+    def swap_state(self, ch_x, ch_y):
+        conv_char = self.state[ch_y * 3 + ch_x]
         self.state = self.state.replace('0', 'X')
-        self.state = self.state.replace(tmp, '0')
-        self.state = self.state.replace('X', tmp)
-        self.sp_x, self.sp_y = x2, y2
+        self.state = self.state.replace(conv_char, '0')
+        self.state = self.state.replace('X', conv_char)
+        self.spc_x, self.spc_y = ch_x, ch_y
 
     def is_correct(self):
         return self.state == CORRECT_STATE
@@ -44,9 +44,8 @@ class PuzzleState:
 
 
 move = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-
-def move_space(puzzle: PuzzleState):
-    mem_state = [puzzle.state]
+def puzzle8_breadth_first_search(puzzle: PuzzleState):
+    mem_state = {puzzle.state: ''}
     p_queue = deque()
     p_queue.append(puzzle)
 
@@ -56,36 +55,28 @@ def move_space(puzzle: PuzzleState):
             #print("CORRECT")
             return u.depth
         for mx, my in move:
-            space_new_x = u.sp_x + mx
-            space_new_y = u.sp_y + my
-
-            if not(0 <= space_new_x <= 2 and 0 <= space_new_y <= 2):
+            sp_new_x = u.spc_x + mx
+            sp_new_y = u.spc_y + my
+            if not(0 <= sp_new_x <= 2 and 0 <= sp_new_y <= 2):
                 continue
-            tmp = PuzzleState(u.state, depth=u.depth+1)
-            tmp.swap_state(tmp.sp_x, tmp.sp_y, space_new_x, space_new_y)
+            new_puzzle = PuzzleState(u.state, depth=u.depth+1)
+            new_puzzle.swap_state(sp_new_x, sp_new_y)
 
-            idx = bisect.bisect_left(mem_state, tmp.state)
-            if idx == len(mem_state) or mem_state[idx] != tmp.state:
-                mem_state.insert(idx, tmp.state)
-                #bisect.insort_left(mem_state, tmp.state)
-                p_queue.append(tmp)
+            if new_puzzle.state not in mem_state:
+                mem_state[new_puzzle.state] = ''
+                p_queue.append(new_puzzle)
     return False
 
 
-def solve_puzzle(input_state):
-    return move_space(PuzzleState(input_state))
-
-
 def main():
-    intext =""
+    input_text = ""
     for i in range(3):
-        intext += "".join(input().split())
+        input_text += "".join(input().split())
 
-    # 8 puzzle
-    ans_puzzle = solve_puzzle(intext)
+    # 8 puzzle main
+    ans_puzzle = puzzle8_breadth_first_search(PuzzleState(input_text))
     print(ans_puzzle)
-
-
+    return
 
 main()
 # -----------------------------
